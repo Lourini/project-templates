7e5fd5ddf6b0fb087635671743182f7f0ce05ed1
Lourini <61596340+Lourini@users.noreply.github.com>
2022-06-18 21:30:18 +0100

    Update main.py

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import streamlit as st
 from tensorflow.keras.models import load_model
 from tensorflow.keras.models import model_from_json
 from tensorflow.keras.preprocessing.image import img_to_array
-from streamlit_webrtc import webrtc_streamer, VideoTransformerBase
+from streamlit_webrtc import webrtc_streamer, VideoProcessorBase
 
 # load model
 emotion_dict = {0: 'Angry', 1: 'Disgust', 2: 'Fear', 3: 'Happy', 4: 'Sad', 5: 'Surprise', 6: 'Neutral'}
@@ -18,31 +18,31 @@ try:
 except Exception:
     st.write("Error loading cascade classifiers")
 
-def transform(self, frame):
-    img = frame.to_ndarray(format="bgr24")
+class VideoTransformer(VideoProcessorBase):
+    def transform(self, frame):
+        img = frame.to_ndarray(format="bgr24")
 
-     #image gray
-    img_gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-    faces = face_cascade.detectMultiScale(
+        #image gray
+        img_gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+        faces = face_cascade.detectMultiScale(
             image=img_gray, scaleFactor=1.3, minNeighbors=5)
-    for (x, y, w, h) in faces:
-        cv2.rectangle(img=img, pt1=(x, y), pt2=(
-            x + w, y + h), color=(255, 0, 0), thickness=2)
-        roi_gray = img_gray[y:y + h, x:x + w]
-        roi_gray = cv2.resize(roi_gray, (48, 48), interpolation=cv2.INTER_AREA)
-        if np.sum([roi_gray]) != 0:
-            roi = roi_gray.astype('float') / 255.0
-            roi = img_to_array(roi)
-            roi = np.expand_dims(roi, axis=0)
-            prediction = classifier.predict(roi)[0]
-            maxindex = int(np.argmax(prediction))
-            finalout = emotion_dict[maxindex]
-            output = str(finalout)
-        label_position = (x, y)
-        cv2.putText(img, output, label_position, cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2)
+        for (x, y, w, h) in faces:
+            cv2.rectangle(img=img, pt1=(x, y), pt2=(
+                x + w, y + h), color=(255, 0, 0), thickness=2)
+            roi_gray = img_gray[y:y + h, x:x + w]
+            roi_gray = cv2.resize(roi_gray, (48, 48), interpolation=cv2.INTER_AREA)
+            if np.sum([roi_gray]) != 0:
+                roi = roi_gray.astype('float') / 255.0
+                roi = img_to_array(roi)
+                roi = np.expand_dims(roi, axis=0)
+                prediction = classifier.predict(roi)[0]
+                maxindex = int(np.argmax(prediction))
+                finalout = emotion_dict[maxindex]
+                output = str(finalout)
+            label_position = (x, y)
+            cv2.putText(img, output, label_position, cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2)
 
-    return img
-
+        return img
 def main():
     # Face Analysis Application #
     st.title("Real Time Face Emotion Detection Application")
@@ -70,7 +70,7 @@ def main():
     elif choice == "Webcam Face Detection":
         st.header("Webcam Live Feed")
         st.write("Click on start to use webcam and detect your face emotion")
-        webrtc_streamer(key="example", video_transformer_factory=transform)
+        webrtc_streamer(key="example", video_transformer_factory=VideoTransformer)
 
     elif choice == "About":
         st.subheader("About this app")
